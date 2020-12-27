05e1e7c9e640f777be1ff50317435ce42938bcec
766521466@qq.com <766521466@qq.com>
2020-12-27 18:15:55 +0800

    init repo

---
+++ dao/src/main/java/shop/life/dao/ProductInfoDao.java
@@ -0,0 +1,10 @@
+package shop.life.dao;
+
+/**
+ * @author :  zhulongkun20@gmail.com
+ * @create :  12-26-2020 15:15:55
+ * @description :  商品信息数据访问接口
+ * @since :  v1.0
+ */
+public interface ProductInfoDao {
+}
---
+++ dao/src/main/java/shop/life/dao/entity/ProductInfo.java
@@ -0,0 +1,10 @@
+package shop.life.dao.entity;
+
+/**
+ * @author :  zhulongkun20@gmail.com
+ * @create :  12-26-2020 15:17:16
+ * @description :  商品信息实体类
+ * @since :  v1.0
+ */
+public class ProductInfo {
+}
